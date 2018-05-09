0062d7b811f1a586c8d2247d62e5fc9eacbc43cc
Tony Faieta <tfaieta@gmail.com>
2018-05-09 13:06:18 -0400

    Implmentation of WidgetCard
    Made a component for the Tracking Widget

--- src/components/CatchUpWidget/CatchUpWidget.js
+++ src/components/CatchUpWidget/CatchUpWidget.js
@@ -1,17 +1,74 @@
 import React from "react";
-import { Carousel } from 'antd';
+import { Col, Row, Carousel } from 'antd';
+import {WidgetCard} from '../../components/WidgetCard/WidgetCard'
 import "./style.scss";
 
 export class CatchUpWidget extends React.Component {
     render() {
         return (
             <div className={"tsHeaderTitles"}>
-                <h2>Tracked Podcasts</h2>
-                <Carousel dots="false"  autoplay>
-                    <div><h3>1</h3></div>
-                    <div><h3>2</h3></div>
-                    <div><h3>3</h3></div>
-                    <div><h3>4</h3></div>
+                <h2 >Tracked Podcasts</h2>
+                <Carousel dots="false">
+                    <div>
+                        <div>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <Row gutter={16}>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                                <Col span={8}>
+                                    <WidgetCard/>
+                                </Col>
+                            </Row>
+                        </div>
+                    </div>
                 </Carousel>
             </div>
         );
@@ -20,5 +77,9 @@ export class CatchUpWidget extends React.Component {
 
 export default CatchUpWidget;
 
+// Tabs I was on
+// https://ant.design/components/card/
+// https://ant.design/docs/react/getting-started
+// TODO: All notes are in the notebook
 
 
---
+++ src/components/WidgetCard/WidgetCard.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Card, Icon } from 'antd';
+import "./style.scss";
+
+const { Meta } = Card;
+
+export class WidgetCard extends React.Component {
+    render() {
+        return (
+            <div>
+                <Card
+                    style={{ width: 350 }}
+                    cover={<img alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                    actions={[<Icon type="play-circle" />, <Icon type="plus" />, <Icon type="cloud-download" />]}
+                >
+                    <Meta
+                        title="Podcast Title"
+                        description="This is the description"
+                    />
+                </Card>
+            </div>
+        );
+    }
+}
+
+export default WidgetCard;
