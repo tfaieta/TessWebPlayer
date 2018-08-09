2bf6b5cbad69548b0d3865a6b04306ff97fcb943
Tony Faieta <tfaieta@gmail.com>
2018-08-09 16:04:42 -0400

    added img to login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {LoginForm} from '../../components/LoginForm/LoginForm'
+import tessLogoWhite from '../../images/tessLogoWhite.png'
 
 export class Login extends React.Component {
     
       render() {
         return (
         <div className="loginContent">
+            <img 
+            alt="logo"
+            style={{height: 142.575, width: 126.575, marginBottom: 50}}
+            src={tessLogoWhite} 
+            />
             <LoginForm/>    
         </div>
         );
