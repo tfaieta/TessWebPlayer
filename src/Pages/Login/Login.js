f6601d1296b518408dfb1ad1ad63a54ce1aec402
Tony Faieta <tfaieta@gmail.com>
2018-08-09 14:15:04 -0400

    more styling

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -5,15 +5,9 @@ export class Login extends React.Component {
     
       render() {
         return (
-          <div className="tcontent">
-            <div className="tsscrollcontent">
-                <div className="container">
-                    <div className="trow-header">
-                        <LoginForm/>
-                    </div>
-                </div>
-            </div>
-          </div>
+        <div className="loginContent">
+            <LoginForm/>    
+        </div>
         );
       }
 }
